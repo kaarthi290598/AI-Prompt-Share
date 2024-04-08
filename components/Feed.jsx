3e978e33be8c97ae5f62b8e1ddb1e31dda4ebafa
kaarthi290598 <kaarthi.work@gmail.com>
2024-04-09 00:07:56 +0530

    cache changes

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import PromptCard from "./PromptCard";
 import { usePrompts } from "@apiServices/useprompt";
